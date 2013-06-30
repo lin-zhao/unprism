e3bd68f1989689ed8b89895f613243a90f0e7b46
Lin Zhao <lin@groupon.com>
2013-06-30 00:13:59 -0700

    Finished!

--- unprism/app/controllers/send_mail_controller.rb
+++ unprism/app/controllers/send_mail_controller.rb
@@ -1,4 +1,6 @@
 require 'openssl'
+require 'base64'
+require 'net/smtp'
 class SendMailController < ApplicationController
   def index
     puts params.inspect
@@ -6,10 +8,26 @@ class SendMailController < ApplicationController
        rsa = OpenSSL::PKey::RSA.generate(2048)
        params["private_key"] = rsa.to_s
        params["public_key"] = rsa.public_key.to_s
+    elsif params["commit"] == "Send Email"
+       do_send
     end    
   end
 
-  def do_send
-    
+  def do_send      
+      key = OpenSSL::PKey::RSA.new(params["private_key"])    
+      plain_text = "Subject:" + params["subject"] + "\n" + params["body"];
+      encrypted = key.private_encrypt(plain_text)
+      encoded = Base64.encode64(encrypted)
+      
+      message = encoded
+
+#      smtp = Net::SMTP.new( params["hostname"], params["port"].to_i)
+#      smtp.enable_tls
+#      smtp.starttls
+#      smtp.send_message message, params["from"], [params["to"]]
+#     smtp.quit
+
+      UnprismMailer.email(params, message).deliver
+
   end   
 end
--- unprism/config/routes.rb
+++ unprism/config/routes.rb
@@ -6,6 +6,7 @@ Unprism::Application.routes.draw do
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
   match 'send_mail' => 'send_mail#index'
+  match 'decrypt' => 'decrypt#decrypt'
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
